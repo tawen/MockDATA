b8a43d456a7f2b4be6c006c8c5840ab9b0e665bb
刘振华 <tawen@live.cn>
2018-06-28 09:53:34 +0800

    add access controll allow

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ app.engine('jsx', reactViews.createEngine());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+//设置跨域访问
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
+
 app.get('/', function (req, res, next) {
   res.render('index', { title:'admin',msg: "@tawen"});
 });
